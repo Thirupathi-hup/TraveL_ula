bb5817dd53c109a8dd78fcd4222b032b2a47e615
siddharth <siddharth@scaler.com>
2024-10-29 22:28:31 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Products from './components/Products/Products';
+import { useState } from 'react';
+import CartContext from './context/CartContext';
+import FullCart from './components/FullCart/FullCart';
 function App() {
+  let [cart, setCart] = useState({});
+
+  function addToCart(product) {
+    const newCart = { ...cart }; // copy of state
+    if (!newCart[product.id]) {
+      newCart[product.id] = { ...product, quantity: 0 };
+    }
+    newCart[product.id].quantity += 1;
+    setCart(newCart);
+
+    console.log(product);
+  }
+
+  function removeFromCart(product) {
+    console.log(product);
+    const newCart = { ...cart };
+    if (!newCart[product.id]) return;
+    newCart[product.id].quantity -= 1;
+    if (newCart[product.id].quantity <= 0) {
+      delete newCart[product.id];
+    }
+    setCart(newCart);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CartContext.Provider value={{cart, addToCart, removeFromCart}}>
+      <div className="App">
+        <FullCart />
+        <Products />
+
+      </div>
+    </CartContext.Provider>
   );
 }
 
 export default App;
+
+
+// ProductCard()
+
+// cart = {1:{id: 1, title: x, quantity: 2}, 
+//  2: { id: 2, title: x1, quantity: 1 }, 
+//  3: { id: 3, title: x2, quantity: 3 }, 
+//  4: { id: 4, title: x3, quantity: 4 }
+//}
+// product = { id: 2, title: x1, quantity: 1 }
+// cart[product.id]
